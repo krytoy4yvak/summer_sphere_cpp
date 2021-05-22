754964f523047a0eef8f33c726b148f7c9d8d458
krytoy4yvak <user02076@gmail.com>
2021-05-22 18:05:00 +0300

    add tests

--- 08/test.cpp
+++ 08/test.cpp
@@ -1,58 +1,37 @@
  #include "threadpool.hpp"
  #include <iostream>
+ #include <vector>
+ #include <sstream>
 
 
-using tcase = int (*)();
-std::vector<tcase> testCases = 
-    {
-        []() 
-        {
-            ThreadPool pool(8);
-            int a = 42;
-            auto task1 = [a](int c) { return a*c; };
-            auto res = pool.exec(task1,2);
-            int answer = 84;
-            if (res.get() != answer){
-                return 1;
-            }
-            return 0;
-        },
-        []() 
-        {
-            ThreadPool pool(8);
-            int a = 42;
-            int b = -10;
-            auto task2 = [a,b]() { return a + b; };            
-            auto res = pool.exec(task2);
-            int answer = 32;
-            if (res.get() != answer){
-                return 1;
-            }
-            return 0;
-        },
-    };
-
-int main(int argc, char* argv[]){
-    if (argc != 1){
-        printf("test run incorrectly\n");
-        return 1;
-    }
-
-    printf("\nTesting started...\n");
-    int testCounter = 0;
-    int status;
-    for (auto& func : testCases)
-    {
-        testCounter++;
-        status = func();
-        if (status != 0)
-        {
-            printf("Test number (%i) failed with status: %i\n", testCounter, status);   
-            return 1;
-        }
-    }
-    printf("All tests passed!\n");   
-    return 0;
+void Test_1()
+{
+	ThreadPool pool(8);
+	int a = 42;
+	auto task1 = [a](int c) { return a*c; };
+	auto result = pool.exec(task1,2);
+	assert(result.get() == 84);
 }
 
 
+void Test_2()
+{
+	ThreadPool pool(8);
+	int a = 42;
+	int b = -10;
+	auto task2 = [a, b]() { return a + b; };            
+	auto result = pool.exec(task2);
+	assert(result.get() == 32);
+}
+
+int main()
+
+{
+	Test_1();
+	Test_2();
+	
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
