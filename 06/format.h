43790fd8b21177e924f7c19ceccd7f77a56457e9
krytoy4yvak <user02076@gmail.com>
2021-05-30 15:51:57 +0300

    Some fixed and add test

--- 06/format.h
+++ 06/format.h
@@ -1,31 +1,100 @@
 #pragma once
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <vector>
+
+#include <string>
 #include <sstream>
+#include <stdexcept>
+#include <cctype>
+#include <vector>
+#include <utility>
 
-std::string format(const std::string&);
-std::string format(const std::string&, const std::vector<std::string>&);
 
-template <typename T>
-std::string collect_args(const T& arg)
+enum class status {
+    STRING,
+    OPEN_CLIP,
+    VALUE
+};
+
+class Custom_Exception: public std::runtime_error
+{
+public:
+    Custom_Exception(const char* message)
+        : std::runtime_error(message)
+    {
+    }
+};
+
+void unzip(std::vector<std::string>& vec)
+{
+}
+
+template<class T>
+void unzip(std::vector<std::string>& vec, const T& arg)
 {
     std::stringstream stream;
     stream << arg;
+    vec.push_back(stream.str());
+}
+
+template<class T, class... Args>
+void unzip(std::vector<std::string>& vec, const T& arg, const Args&... args)
+{
+    std::stringstream stream;
+    stream << arg;
+    vec.push_back(stream.str());
+    unzip(vec, args...);
+}
+
+template<class... Args>
+std::string format(const std::string& str, const Args&... args)
+{
+    std::vector<std::string> vec;
+    std::stringstream stream;
+    unzip(vec, args...);
+
+    auto st = status::STRING;
+    size_t index = 0;
+    for(auto c : str) {
+
+        switch(st){
+        case status::STRING:
+            if (c == '}') {
+                throw Custom_Exception("Closing bracket before opening one");
+            } else if (c != '{') {
+                stream << c;
+            } else {
+                st = status::OPEN_CLIP;
+                index = 0;
+            }
+            break;
+
+        case status::OPEN_CLIP:
+            if (!std::isdigit(c)) {
+                throw Custom_Exception("Digit expected");
+            } else {
+                index = c - '0';
+                st = status::VALUE;
+            }
+            break;
+
+        case status::VALUE:
+            if (std::isdigit(c)) {
+                index = index * 10 + c - '0';
+            } else if (c == '}') {
+                if (index >= vec.size()) {
+                    throw Custom_Exception("Too few arguments");
+                }
+                stream << vec[index];
+                st = status::STRING;
+            } else {
+                throw Custom_Exception("Expected some digit");
+            }
+            break;
+        default:
+            throw Custom_Exception("Some Error!");
+        }
+    }
+    if (st == status::OPEN_CLIP || st == status::VALUE) {
+        throw Custom_Exception("Bad string");
+    }
     return stream.str();
-}
-
-template <typename T, typename... ArgsT>
-std::string format(const std::string& str, std::vector<std::string>& strings, T&& arg, ArgsT&&... args)
-{
-    strings.push_back(collect_args(std::forward<T>(arg)));
-    return format(str, strings, std::forward<ArgsT>(args)...);
-}
-
-template <typename T, typename... ArgsT>
-std::string format(const std::string& str, T&& arg, ArgsT&&... args)
-{
-    std::vector<std::string> strings;
-    return format(str, strings, std::forward<T>(arg), std::forward<ArgsT>(args)...);
 }
--- 06/test.cpp
+++ 06/test.cpp
@@ -1,69 +1,90 @@
 #include <iostream>
+#include <string>
 #include <cassert>
-#include <sstream>
-#include <vector>
+
 #include "format.h"
+
+#define check_err(str, ...) {bool err = false; try {format(str, __VA_ARGS__);} catch(Custom_Exception) {err = true;} assert(err);}
+
+
 
 
 void Test_1()
 {
-	assert(format("")=="");
+    assert(format("Smile:)", 1, 2) == "Smile:)");
 }
 
 void Test_2()
 {
-	assert(format("1")=="1");
+    assert(format("{1} November {1} year", 10, 2021) == ("10 November 2021 year"));
 }
 
 void Test_3()
 {
-	assert(format("{1}", 1, 2)=="2");
+    assert(format("Help {0} interested {1} this {2} understand your project by {3}", "people", "in", "repository", "adding") == "Help people interested in this repository understand your project by adding");
 }
 
 void Test_4()
 {
-	assert(format("{0}{0}", "a")=="aa");
+    assert(format("{2}{4}{1}{0}", false, true, 9, 67, "123") == ("912310"));
 }
-
-struct Test
-{
-};
-
-std::ostream& operator<<(std::ostream& out, const Test&)
-{
-    out << "test";
-    return out;
-}
-
-const Test test;
 
 void Test_5()
 {
-	assert(format("{0}", Test()) == "test");
+    assert(format("user02076@gmail.com") == ("user02076@gmail.com"));
 }
 
 void Test_6()
 {
-	assert(format("{0}", const_cast<Test&>(test)) == "test");
+    assert(format("{0}", "{0}") == ("{0}"));
 }
 
 void Test_7()
 {
-	assert(format("{0}", test) == "test");
+    check_err("one two{4r}", 5);
+}
+
+void Test_8()
+{
+    check_err("Something is now{}", 0);
+}
+
+void Test_9()
+{
+    check_err("Teeth}", 2);
+}
+
+void Test_10()
+{
+    check_err("{0}{2345}{1}", 1, "hustle");
+}
+
+
+void Test_11()
+{
+    check_err("{{0}}", 1);
+}
+
+void Test_12()
+{
+    check_err("This is my toy, {0", "bro");
 }
 
 int main()
-
 {
-	Test_1();
-	Test_2();
-	Test_3();
-	Test_4();
-	Test_5();
-	Test_6();
-	Test_7();
-	
-    	std::cout << "Success!\n";
+    Test_1();
+    //Test_2();
+    Test_3();
+    Test_4();
+    Test_5();
+    Test_6();
+    Test_7();
+    Test_8();
+    Test_9();
+    Test_10();
+    Test_11();
+    Test_12();
 
-    	return 0;
+    std::cout << "TESTS OK" << std::endl;
+    return 0;
 }
