6d9ca2536944697be081d754997fce08381f979a
krytoy4yvak <user02076@gmail.com>
2021-05-30 14:16:44 +0300

    Union all in one file and add more tests

--- 05/test.cpp
+++ 05/test.cpp
@@ -1,108 +1,172 @@
-#include <cassert>
 #include <iostream>
+#include <cassert>
 #include <sstream>
-#include "bigint.h"
 
+#include "serializer.h"
 
+struct Data
+{
+    uint64_t a;
+    bool b;
+    uint64_t c;
+
+    Data(uint64_t a, bool b, uint64_t c)
+        : a(a)
+        , b(b)
+        , c(c)
+    {
+    }
+
+    template <class S>
+    Error serialize(S& serializer) const
+    {
+        return serializer(a, b, c);
+    }
+    template <class D>
+    Error deserialize(D& deserializer)
+    {
+        Error error = Error::NoError;
+        if ((error = deserializer.load(a)) != Error::NoError) {
+            return error;
+        }
+        if ((error = deserializer.load(b)) != Error::NoError) {
+            return error;
+        }
+        return deserializer.load(c);
+    }
+};
 
 
 void Test_1()
 {
-	BigInt bint_1 = "-42";
-	BigInt bint_2 = "10";
-	BigInt result = bint_1 + bint_2;
-	assert(result == "1");
+    Data x(956, true, 865);
+    std::stringstream stream;
+    Serializer serializer(stream);
+    serializer.save(x);
+
+    Data y { 4321, false, 7656 };
+    Deserializer deserializer(stream);
+    const Error err = deserializer.load(y);
+    assert(err == Error::NoError);
+
+    assert(x.a == y.a);
+    assert(x.b == y.b);
+    assert(x.c == y.c);
+
+
 }
 
 void Test_2()
 {
-	BigInt bint_1 = "-42";
-	BigInt bint_2 = "10";
-	BigInt result = bint_1 - bint_2;
-	assert(result == "-52");
+    Data x(956, true, 865);
+    std::stringstream stream;
+    Serializer serializer(stream);
+    serializer.save(x);
+
+    Data y { 4321, false, 7656 };
+    Deserializer deserializer(stream);
+
+    auto s = std::stringstream("");
+    Deserializer d(s);
+    assert(d.load(y) == Error::CorruptedArchive);
+
+
 }
 
 void Test_3()
 {
-	BigInt bint_1 = "-42";
-	BigInt bint_2 = "10";
-	bool result = bint_1 < bint_2;
-	assert(result == true);
+    Data x(956, true, 865);
+    std::stringstream stream;
+    Serializer serializer(stream);
+    serializer.save(x);
+
+    Data y { 4321, false, 7656 };
+    Deserializer deserializer(stream);
+
+    auto s = std::stringstream("1 text 3");
+    Deserializer d(s);
+    assert (d.load(y) == Error::CorruptedArchive);
+
+
 }
 
 void Test_4()
 {
-	BigInt bint_1 = "-42";
-	BigInt bint_2 = "10";
-	bool result = bint_1 > bint_2;
-	assert(result == false);
+    Data x(956, true, 865);
+    std::stringstream stream;
+    Serializer serializer(stream);
+    serializer.save(x);
+
+    Data y { 4321, false, 7656 };
+    Deserializer deserializer(stream);
+
+    auto s = std::stringstream("9999 1");
+    Deserializer d(s);
+    assert (d.load(y) == Error::CorruptedArchive);
+
+
 }
 
 void Test_5()
 {
-	BigInt bint_1 = "-42";
-	BigInt bint_2 = "10";
-	bool result = bint_1 <= bint_2;
-	assert(result == true);
+    Data x(956, true, 865);
+    std::stringstream stream;
+    Serializer serializer(stream);
+    serializer.save(x);
+
+    Data y { 4321, false, 7656 };
+    Deserializer deserializer(stream);
+
+    auto s = std::stringstream("654321 true 7658");
+    Deserializer d(s);
+    assert (d.load(y) == Error::NoError);
+
+    assert(y.a == 654321);
+    assert(y.b == true);
+    assert(y.c == 7658);
+
+
 }
 
 void Test_6()
 {
-	BigInt bint_1 = "-42";
-	BigInt bint_2 = "-42";
-	bool result = bint_1 <= bint_2;
-	assert(result == true);
+    Data x(956, true, 865);
+    std::stringstream stream;
+    Serializer serializer(stream);
+    serializer.save(x);
+
+    Data y { 4321, false, 7656 };
+    Deserializer deserializer(stream);
+    auto s = std::stringstream("-654321 true 7658");
+    Deserializer d(s);
+    assert (d.load(y) == Error::CorruptedArchive);
 }
+
 
 void Test_7()
 {
-	BigInt bint_1 = "-42";
-	BigInt bint_2 = "10";
-	bool result = bint_1 >= bint_2;
-	assert(result == false);
+    Data x(956, true, 865);
+    std::stringstream stream;
+    Serializer serializer(stream);
+    serializer.save(x);
+
+    Data y { 4321, false, 7656 };
+    Deserializer deserializer(stream);
+    auto s = std::stringstream("987654321234567898765432 false 876");
+    Deserializer d(s);
+    assert (d.load(y) == Error::CorruptedArchive);
 }
 
-void Test_8()
-{
-	BigInt bint_1 = "-42";
-	BigInt bint_2 = "-42";
-	bool result = bint_1 >= bint_2;
-	assert(result == true);
-}
-
-void Test_9()
-{
-	BigInt bint_1 = "-42";
-	BigInt result = -bint_1;
-	assert(result == "42");
-}
-
-void Test_10()
-{
-	std::stringstream stream;
-	BigInt bint_1 = "-42";
-	std::string result = "0";
-	stream << bint_1;
-	stream >> result;
-	assert(result == "-42");
-}
 
 int main()
-
 {
-	Test_1();
-	Test_2();
-	Test_3();
-	Test_4();
-	Test_5();
-	Test_6();
-	Test_7();
-	Test_8();
-	Test_9();
-	Test_10();
-	
-
-    std::cout << "Success!\n";
-
+    Test_1();
+    Test_2();
+    Test_3();
+    Test_4();
+    Test_5();
+    Test_6();
+    Test_7();
+    std::cout << "TESTS OK" << std::endl;
     return 0;
 }
